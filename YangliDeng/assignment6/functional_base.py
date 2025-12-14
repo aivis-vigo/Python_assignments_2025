b9807dcb1f06da9ff8e52b6baf4b635ca781159b
Yangli Deng <dengyangli313@gmail.com>
2025-12-14 21:44:20 +0200

    Add assignment 6 by Yangli Deng

---
+++ YangliDeng/assignment6/functional_base.py
@@ -0,0 +1,14 @@
+# Task 1
+def generate_full_name(first, last):
+    return first + " " + last
+
+# Task 2
+def calculate_average(grades):
+    return sum(grades) / len(grades)
+
+full_name = generate_full_name("Ivan", "Petrov")
+print("Full name:", full_name)
+
+grades = [80, 90, 85]
+average = calculate_average(grades)
+print("Average grade:", average)
